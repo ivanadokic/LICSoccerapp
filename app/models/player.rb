539dbbf90fc28867c96e6091fe428b8f8451706c
Ivana <ivana_pile@yahoo.com>
2020-02-13 10:08:23 -0500

    set validates :username

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,7 +2,8 @@ class Player < ActiveRecord::Base
     has_many :schedules
     belongs_to :team
     has_many :events, through: :schedules
-
+    
+    validates :username, uniqueness: { message: "is already in use. Try again!"}
     validates :age, presence: true
     validates :age, numericality: { greater_than: 5, less_than: 16 }
     
