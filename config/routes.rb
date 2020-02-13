68d7e842293065b98aaaa90207b4872f7998c148
Ivana <ivana_pile@yahoo.com>
2020-02-13 09:42:39 -0500

    set routes welcome, login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/register', to: 'players#new'
-  post '/players' , to: 'players#create'
-  get '/login', to: 'sessions#new'
-  post '/sessions', to: 'sessions#create'
 
-  root 'application#home'
+
+
+
+ 
+
+  resources :players, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
 end
