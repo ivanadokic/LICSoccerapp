ee724b9e2c1bd4e7e4c8467506eeb149465aeb59
Ivana <ivana_pile@yahoo.com>
2020-02-13 15:21:56 -0500

    assign_team method

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
     has_many :schedules
-    belongs_to :team
+    belongs_to :team, optional: true
     has_many :events, through: :schedules
     
     validates :username, uniqueness: { message: "is already in use. Try again!"}
@@ -8,4 +8,13 @@ class Player < ActiveRecord::Base
     validates :age, numericality: { greater_than: 5, less_than: 16 }
     
     has_secure_password
+
+    def assign_team
+        t = Team.find_by_age(self.age + 1) 
+        if t.nil?
+        t = Team.find_by_age(self.age + 2)  
+        end
+        self.team = t
+        self.save
+    end
 end
