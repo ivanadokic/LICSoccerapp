cfb357a4a95d6c1837b8db45c6e5c6cfff47d584
Ivana <ivana_pile@yahoo.com>
2020-02-13 09:44:08 -0500

    logged_in method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def current_player    
+        Player.find_by(id: session[:player_id])  
+      end
+  
+      def logged_in?
+          !current_player.nil?  
+      end
+      
 end
