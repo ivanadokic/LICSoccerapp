eca10c9517346636c3b32da7fbf6a08fd1dad459
Ivana <ivana_pile@yahoo.com>
2020-02-14 09:44:27 -0500

    syntax error fixed

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,10 +5,14 @@ class PlayersController < ApplicationController
     end
 
     def create 
-        @player = Player.create (params.require(:player).permit(:username, :password))
-
+        @player = Player.create (params.require(:player).permit(:username, :password, :age))
+        if @player.valid? 
         session[:player_id] = @player.id
+        @player.assign_team
         redirect_to '/welcome'
+        else
+            render :new
+        end
     end
 
 end
