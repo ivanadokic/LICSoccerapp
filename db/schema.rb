1e421403678e65dbbee3cf8966f59a2c873d9286
Ivana <ivana_pile@yahoo.com>
2020-02-17 11:55:07 -0500

    omnioauth file created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_12_172926) do
+ActiveRecord::Schema.define(version: 2020_02_17_151356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_02_12_172926) do
     t.integer "age"
     t.integer "team_id"
     t.string "password_digest"
+    t.string "uid"
   end
 
   create_table "schedules", force: :cascade do |t|
