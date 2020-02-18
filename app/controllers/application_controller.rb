d85b74c73e06c567cbe1fdb8e50e521868242f65
Ivana <ivana_pile@yahoo.com>
2020-02-18 11:04:04 -0500

    commented out sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     #before_action :authorized #The macro before_action requires the authorized method to run before any other action is taken. 
     include ApplicationHelper
 
-    enable :sessions
+    #enable :sessions
     set :session_secret, "super_secret_code_word" #encript our sessions
 
     def home
