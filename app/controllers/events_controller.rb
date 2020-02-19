6835ce4b4b86c52b69b512716889488d89b048bd
Ivana <ivana_pile@yahoo.com>
2020-02-19 10:31:40 -0500

    added update destroy edit to event_controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,44 @@
 class EventsController < ApplicationController
-    def index
-    end
-
     def new
         @event=Event.new
     end
-
 
     def create
         @event = Event.create (params.permit(:event).permit(:location, :event_type, :start, :end))
         #removed require and put permit
         @event.save 
         if @event.save 
-            redirect_to '/welcome'
+           redirect_to event_path(@event)
         else
             render :new
         end
     end
 
+    def index
+        if params[:event_id] && @event = Event.find_by_id(params[:team_id])
+            @events = @event.team
+           
+        else
+            @events = Event.all
+        end
+    end
+    def edit
+        @event = Event.find(params[:id])
+    end
+    def update
+        @event = Event.find(params[:id])
+        @event.update(params.require(:event).permit(location, :event_type, :start, :end))
+        redirect_to @event
+      end
+    
+      def destroy  
+          @event = Event.find(params[:id])
+          @event.destroy
+          redirect_to event_path
+      end
+    
+      def show
+        @event = Event.find(params[:id])
+      end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root :to => "welcome#home"
 
   resources :players, only: [:new, :create]
-  resources :events, only: [:show, :new, :create]
+  resources :events, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+
   resources :teams, only: [:new, :create]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#welcome'
@@ -12,7 +14,10 @@ Rails.application.routes.draw do
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
   get 'register', to: 'players#new'
-  get 'events', to: 'events#create'
+  get '/events', to: 'events#index'
+  get '/events', to: 'events#create'  
+  get '/events/new', to: 'events#new'
+ 
  
   
   
