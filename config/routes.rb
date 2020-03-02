ffc4a116b99a6e84de149c97f15c901774df0c21
Ivana <ivana_pile@yahoo.com>
2020-03-02 10:22:15 -0500

    strong params

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root :to => "welcome#home"
-  
   resources :events do
   resources :schedules, only: [:index, :new, :create] 
  
   end
 
-  resources :players, only: [:new, :create, :logout]
+  resources :players, only: [:new, :create]
   
   #unnested routes
  
